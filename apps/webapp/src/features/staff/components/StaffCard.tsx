d32150cd727b6e16f2f59714680c04de2088b7fa
Jomel Ortega <jomel.ortega.systech88@gmail.com>
2025-08-25 15:07:12 +0800

    style(StaffCard): improve layout and spacing for better readability

--- apps/webapp/src/features/staff/components/StaffCard.tsx
+++ apps/webapp/src/features/staff/components/StaffCard.tsx
@@ -27,9 +27,9 @@ const StaffCard: React.FC<Props> = ({ staff, onViewStaff, onDeleteStaff }) => {
   return (
     <div
       key={staff.id}
-      className="bg-white rounded-lg shadow p-4 flex flex-col gap-3 border relative"
+      className="bg-white rounded-lg shadow flex flex-col border relative"
     >
-      <div className="flex items-center gap-3">
+      <div className="flex items-center gap-3 p-4 border-b">
         <Avatar className="size-12">
           <AvatarImage
             src={`${import.meta.env.VITE_PUBLIC_CDN_URL}${staff.avatar?.url}`}
@@ -59,74 +59,75 @@ const StaffCard: React.FC<Props> = ({ staff, onViewStaff, onDeleteStaff }) => {
           </div>
         </div>
       </div>
-      <div className="flex gap-2 items-center">
-        <span className="text-xs text-gray-600 uppercase font-bold inline-flex items-center gap-1">
-          <IdCard className="size-3.5" />
-          Employment:
-        </span>
-        {EMPLOYMENT_TYPE_BADGES[staff.employmentType]}
-      </div>
-
-      {staff.assignedServices && staff.assignedServices.length > 0 && (
-        <div className="flex gap-2 items-baseline">
-          <div className="w-[80px] text-xs text-gray-600 uppercase font-bold inline-flex items-center gap-1">
-            <BriefcaseMedical className="size-3.5" />
-            Services:
-          </div>
-          <ul className="text-gray-950 list-disc w-[calc(100%-100px)]">
-            {staff.assignedServices.slice(0, 3).map((s, index) => (
-              <li key={s.id} className="truncate flex-1 pl-3 text-sm">
-                • {s.name}{' '}
-                {index === 2 && !!(staff.assignedServices.length - 3) ? (
-                  <>
-                    <Tooltip>
-                      <TooltipTrigger
-                        asChild
-                        className="bg-transparent text-primary text-xs font-bold"
-                      >
-                        <span className="ml-1 cursor-default">
-                          +{staff.assignedServices.length - 3}
-                        </span>
-                      </TooltipTrigger>
-                      <TooltipContent
-                        side="top"
-                        align="center"
-                        className="bg-gray-200 text-shadow-gray-800 [&>span>svg]:bg-gray-200 [&>span>svg]:fill-gray-200"
-                      >
-                        <ul className="text-gray-950 list-disc pl-3">
-                          {staff.assignedServices.slice(3).map((treatment) => (
-                            <li key={treatment.id} className="text-sm">
-                              {treatment.name}
-                            </li>
-                          ))}
-                        </ul>
-                      </TooltipContent>
-                    </Tooltip>
-                  </>
-                ) : (
-                  ''
-                )}
-              </li>
-            ))}
-          </ul>
-        </div>
-      )}
-
-      {staff.workSchedules && staff.workSchedules.length > 0 && (
-        <div className="text-sm text-gray-700 flex gap-2 mt-auto">
+      <div className="flex flex-col gap-3 p-4 flex-1">
+        <div className="flex gap-2 items-center">
           <span className="text-xs text-gray-600 uppercase font-bold inline-flex items-center gap-1">
-            <Calendar className="size-3.5" />
-            Schedules:
+            <IdCard className="size-3.5" />
           </span>
-          {renderWorkingDays(staff.workSchedules)}
+          {EMPLOYMENT_TYPE_BADGES[staff.employmentType]}
         </div>
-      )}
-      <div className="absolute top-4 right-4">
-        <RenderStaffActions
-          staffId={staff.id}
-          onView={onViewStaff}
-          onDelete={onDeleteStaff}
-        />
+
+        {staff.assignedServices && staff.assignedServices.length > 0 && (
+          <div className="flex gap-2 items-baseline">
+            <div className="text-xs text-gray-600 uppercase font-bold inline-flex items-center gap-1 relative top-0.5">
+              <BriefcaseMedical className="size-3.5" />
+            </div>
+            <ul className="text-gray-950 list-disc w-[calc(100%-20px)]">
+              {staff.assignedServices.slice(0, 3).map((s, index) => (
+                <li key={s.id} className="truncate flex-1 text-[13px]">
+                  • {s.name}{' '}
+                  {index === 2 && !!(staff.assignedServices.length - 3) ? (
+                    <>
+                      <Tooltip>
+                        <TooltipTrigger
+                          asChild
+                          className="bg-transparent text-primary text-xs font-bold"
+                        >
+                          <span className="ml-1 cursor-default">
+                            +{staff.assignedServices.length - 3}
+                          </span>
+                        </TooltipTrigger>
+                        <TooltipContent
+                          side="top"
+                          align="center"
+                          className="bg-gray-200 text-shadow-gray-800 [&>span>svg]:bg-gray-200 [&>span>svg]:fill-gray-200"
+                        >
+                          <ul className="text-gray-950 list-disc pl-3">
+                            {staff.assignedServices
+                              .slice(3)
+                              .map((treatment) => (
+                                <li key={treatment.id} className="text-sm">
+                                  {treatment.name}
+                                </li>
+                              ))}
+                          </ul>
+                        </TooltipContent>
+                      </Tooltip>
+                    </>
+                  ) : (
+                    ''
+                  )}
+                </li>
+              ))}
+            </ul>
+          </div>
+        )}
+
+        {staff.workSchedules && staff.workSchedules.length > 0 && (
+          <div className="text-sm text-gray-700 flex gap-2 mt-auto">
+            <span className="text-xs text-gray-600 uppercase font-bold inline-flex items-center gap-1">
+              <Calendar className="size-3.5" />
+            </span>
+            {renderWorkingDays(staff.workSchedules)}
+          </div>
+        )}
+        <div className="absolute top-6 right-4">
+          <RenderStaffActions
+            staffId={staff.id}
+            onView={onViewStaff}
+            onDelete={onDeleteStaff}
+          />
+        </div>
       </div>
     </div>
   );
