eaecf3ce6042a0d86f49c1fb31f4472da7a85829
Jomel Ortega <jomel.ortega.systech88@gmail.com>
2025-08-30 15:11:35 +0800

    style(DialogDrawer, UpdateStaff): adjust positioning and height for improved layout consistency

--- packages/ui/src/components/sheet.tsx
+++ packages/ui/src/components/sheet.tsx
@@ -56,7 +56,7 @@ function SheetContent({
       <SheetPrimitive.Content
         data-slot="sheet-content"
         className={cn(
-          'bg-background data-[state=open]:animate-[enter_0.2s_ease] data-[state=closed]:animate-[exit_0.3s_ease] fixed z-50 flex flex-col gap-4 shadow-lg transition ease-in-[exit_0.3s_ease] data-[state=closed]:duration-300 data-[state=open]:duration-500',
+          'bg-background fixed! data-[state=open]:animate-[enter_0.2s_ease] data-[state=closed]:animate-[exit_0.3s_ease] z-50 flex flex-col gap-4 shadow-lg transition ease-in-[exit_0.3s_ease] data-[state=closed]:duration-300 data-[state=open]:duration-500',
           side === 'right' &&
             'data-[state=closed]:slide-out-to-right data-[state=open]:slide-in-from-right inset-y-0 right-0 h-full w-3/4 border-l sm:max-w-sm',
           side === 'left' &&
